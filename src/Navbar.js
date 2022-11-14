fdf6bbb2adca1460536a82c5c0008a902cdee597
Hillary Odo <Hillary.Odo@mainone.net>
2022-11-14 21:49:30 +0000

    fix: href fixed

--- src/Navbar.js
+++ src/Navbar.js
@@ -8,8 +8,8 @@ export default function Navbar({open}){
         <ul className="navbar">
             <li><a href="/">Home</a></li>
             <li><a href="/placeToStay">Place to stay</a></li>
-            <li><a href="#">NFTs</a></li>
-            <li><a href="#">Community</a></li>
+            <li><a>NFTs</a></li>
+            <li><a>Community</a></li>
         </ul>
         <button onClick={() => open(true)} className="walletBtn">Connect Wallet</button>
     </nav>   
