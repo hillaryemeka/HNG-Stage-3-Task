f16535083f8315ef5810cdf159f178dfc127f32d
Hillary Odo <Hillary.Odo@mainone.net>
2022-11-14 12:16:00 +0000

    feat: moving the navbar and footer to separate components

---
+++ src/Footer.js
@@ -0,0 +1,53 @@
+
+import React from "react";
+
+export default function Footer(){
+    return(
+        <footer>
+        <div id="foot1">
+            <div id="foot1_meta">
+                <img src="images/image_21.png" alt=""></img>
+                <img src="images/image_22.png" alt="" /> 
+            </div>
+            <div id="socialmedia">
+                <img src="images/facebook.png" alt="" />
+                <img src="images/instagram.png" alt="" />
+                <img src="images/twitter.png" alt="" />
+            </div>
+            <div id="copywright">
+                <img src="images/copyright.png" alt="" />
+                2022 Metabnb
+            </div>
+        </div>
+        <div id="foot2">
+            <div id="community">
+                community
+                <ul id="ul_items">
+                    <li>NFTs</li>
+                    <li>Tokens</li>
+                    <li>Landlords</li>
+                    <li>Discord</li>
+                </ul>
+            </div>
+            <div id="places">
+                Places
+            <ul id="ul_items">
+                    <li>Castle</li>
+                    <li>Farms</li>
+                    <li>Beach</li>
+                    <li>Lerarn more</li>
+                </ul>
+            </div>
+            <div id="about">
+                About us
+            <ul id="ul_items">
+                    <li>Road map</li>
+                    <li>Creators</li>
+                    <li>Career</li>
+                    <li>Conatct us</li>
+                </ul>                    
+            </div>
+        </div>
+    </footer>
+    )
+}
---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+
+import React from "react"
+
+export default function Navbar({open}){
+    return(
+    <nav>
+        <img alt="" src="images/Meta_logo.png" className="logo"/>
+        <ul className="navbar">
+            <li><a href="/">Home</a></li>
+            <li><a href="/placeToStay">Place to stay</a></li>
+            <li><a href="">NFTs</a></li>
+            <li><a href="">Community</a></li>
+        </ul>
+        <button onClick={() => open(true)} className="walletBtn">Connect Wallet</button>
+    </nav>   
+    )
+}
