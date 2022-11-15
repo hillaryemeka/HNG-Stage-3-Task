3e5ff1e5578e5d5fac94549f92077ae1c0786611
Hillary Odo <Hillary.Odo@mainone.net>
2022-11-15 09:22:15 +0000

    fix: netlify issues fix

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar({open}){
         <img alt="" src="images/Meta_logo.png" className="logo"/>
         <ul className="navbar">
             <li><a href="/">Home</a></li>
-            <li><a href="/placeToStay">Place to stay</a></li>
+            <li><a href="/placesostay">Place to stay</a></li>
             <li>NFT</li>
             <li>Community</li>
         </ul>
