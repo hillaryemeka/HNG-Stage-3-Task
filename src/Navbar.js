faf98bf7d7f678bedfdcd8fef55b564d7a93e947
Hillary Odo <Hillary.Odo@mainone.net>
2022-11-15 13:10:53 +0000

    style: some touch on the css to improve responsiveness

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,13 +5,15 @@ import {Link} from "react-router-dom"
 export default function Navbar({open}){
     return(
     <nav>
-        <img alt="" src="images/Meta_logo.png" className="logo"/>
-        <ul className="navbar">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/placetostay">Place to stay</Link></li>
-            <li>NFT</li>
-            <li>Community</li>
-        </ul>
+        <div><img alt="" src="images/Meta_logo.png" className="logo"/></div>
+        <div>
+            <ul className="navbar">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/placetostay">Place to stay</Link></li>
+                <li>NFT</li>
+                <li>Community</li>
+            </ul>
+        </div>
         <button onClick={() => open(true)} className="walletBtn">Connect Wallet</button>
     </nav>   
     )
